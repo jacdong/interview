8d65c11df5dfbe35cd3add84a0dc666dff38023d
DongBin <DongBin@SPC-20210322JHU.sixents.com>
2021-09-13 11:00:49 +0800

    init the setup class

---
+++ micro-interview/micro-interview-admin/src/main/java/com/jacdong/interview/admin/AdminApplcation.java
@@ -0,0 +1,18 @@
+package com.jacdong.interview.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+
+@SpringBootApplication
+public class AdminApplcation
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(AdminApplcation.class, args);
+    }
+}
