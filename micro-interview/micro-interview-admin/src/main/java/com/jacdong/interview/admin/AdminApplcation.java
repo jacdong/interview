0bd096d7f6e4f59096e4e47a5416a7b67d7d6ddb
DongBin <DongBin@SPC-20210322JHU.sixents.com>
2021-09-13 11:34:30 +0800

    add annotation for discovery module

--- micro-interview/micro-interview-admin/src/main/java/com/jacdong/interview/admin/AdminApplcation.java
+++ micro-interview/micro-interview-admin/src/main/java/com/jacdong/interview/admin/AdminApplcation.java
@@ -2,17 +2,16 @@ package com.jacdong.interview.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Hello world!
  *
  */
-
+@EnableDiscoveryClient
 @SpringBootApplication
-public class AdminApplcation
-{
-    public static void main( String[] args )
-    {
-        SpringApplication.run(AdminApplcation.class, args);
-    }
+public class AdminApplcation {
+	public static void main(String[] args) {
+		SpringApplication.run(AdminApplcation.class, args);
+	}
 }
